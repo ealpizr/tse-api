c3bc20fbb7a49bf4a1562bb13a5e95273804c9d8
Enrique Alpízar <69376444+ealpizr@users.noreply.github.com>
2022-03-17 07:45:40 -0600

    Get port from env

--- src/main.go
+++ src/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"os"
 	"github.com/ealpizr/tse-api/src/router"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	port := os.GetEnv("PORT")
+	
 	app := fiber.New()
 	router.RegisterRoutes(app)
 	app.Listen(":80")
